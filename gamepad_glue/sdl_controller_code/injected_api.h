62d1cc88ac5cb8272ec0c0d53edcc2e52bdc494e
robotanarchy <robotanarchy@bingo-ev.de>
2014-12-26 19:30:13 +0100

    rewrite soon-to-be-used header file of injected api

---
+++ gamepad_glue/sdl_controller_code/injected_api.h
@@ -0,0 +1,35 @@
+#pragma once
+
+// Incoming messages
+#define IA_IN_MOVEMENT 0x00
+typedef struct { char movement; } IA_IN_MOVEMENT_t;
+
+#define IA_IN_TEXT 0x01
+typedef enum { PICKUP, NETWORK, BIG } gta2_textpos_t;
+typedef struct {
+  gta2_textpos_t position;
+  char timer;
+  char[100] text;
+} IA_IN_TEXT_t;
+
+// outgoing messages
+#define IA_OUT_PLAYER_COUNT 0x00
+typedef struct { char count; } IA_OUT_PLAYER_COUNT_t;
+
+#define IA_OUT_SCOREBOARD 0x01
+typedef struct {
+  char[6] score;
+  int[6] money;
+} IA_OUT_SCOREBOARD_t;
+
+#define IA_OUT_WINHANDLE 0x02
+typedef struct { int handle; } IA_OUT_WINHANDLE_t;
+
+#define IA_OUT_RUMBLE 0x03
+typedef struct { char byte; } IA_OUT_RUMBLE_t;
+
+#define IA_OUT_SCORE_DIFF 0x04
+typedef struct { int diff; } IA_OUT_SCORE_DIFF_t;
+
+// outgoing events (without arguments)
+#define IA_OUT_QUIT 0xE0
--- gamepad_glue/sdl_controller_code/proxydll_common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define PDLL_MOVEMENT 0xaa
-#define PDLL_ERROR 0xee
-
-typedef struct {
-  char id;   // use a PDLL_* constant from above!
-  short arg; // 32 bit windows: 2 bytes
-} proxydll_msg_t;
