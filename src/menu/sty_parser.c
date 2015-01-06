eda5b45d86cf6fb900b68a6a182083de3ef66450
robotanarchy <robotanarchy@bingo-ev.de>
2015-01-06 15:02:37 +0100

    implement chunk parser, not really working yet

--- src/menu/sty_parser.c
+++ src/menu/sty_parser.c
@@ -1,6 +1,35 @@
 #include "sty_parser.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// offsets in fstyle.sty
+// PPAL: 000006
+// PALX: 065608
+// PALB: 767888
+
+// FIXME
+// returns the next offset or 0
+uint32_t sty_parser_read_next_chunk(char *buffer, uint32_t offset,
+                                    uint32_t sty_size) {
+  // read the chunk type and size
+  char type[5];
+  for (int i = 0; i < 4; i++)
+    type[i] = buffer[offset + i];
+  type[5] = '\0';
+  uint32_t size = (*(uint32_t *)(buffer + offset + 5)) + 8;
+
+  printf("Chunk: %s (size: %i)\n", type, size);
+  printf("sty size:    %i\n", sty_size);
+  printf("Offset:      %i\n", offset);
+  printf("Offset+size: %i\n", offset + size);
+  printf("\n");
+
+  offset += size;
+  if (!size || offset >= sty_size)
+    exit(printf("ERROR!\n"));
+  return offset;
+}
 
 sty_t *sty_parser(char *filename) {
   printf("sty_parser: reading file %s\n", filename);
@@ -28,7 +57,12 @@ sty_t *sty_parser(char *filename) {
       buffer[3] != 'T')
     exit(printf("This isn't a GBST file!\n"));
 
-  // ...
+  uint32_t offset = 6; // skip the header!
+  uint32_t sty_parser_read_next_chunk(char *buffer, uint32_t offset,
+                                      uint32_t sty_size);
+
+  while (offset)
+    offset = sty_parser_read_next_chunk(buffer, offset, size);
 
   return NULL;
 }
