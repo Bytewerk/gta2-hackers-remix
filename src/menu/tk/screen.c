c7214aa4314e7a5ddb339d354b32d4b1500da9ba
robotanarchy <robotanarchy@bingo-ev.de>
2015-04-15 15:39:36 +0200

    honor padding when drawing elements

--- src/menu/tk/screen.c
+++ src/menu/tk/screen.c
@@ -4,6 +4,7 @@
 
 tk_screen_t *tk_screen(tk_t *tk) {
   tk_screen_t *screen = calloc(1, sizeof(tk_screen_t));
+  screen->el.type = STACK;
 
   // attach it to the tk for easy cleanup
   tk_el_t *listpos = (tk_el_t *)tk->screen_first;
@@ -21,11 +22,16 @@ void recursive_draw(tk_t *tk, tk_el_t *el_selected, tk_el_t *el, int offset_x,
                     int offset_y) {
   while (el) {
     int width = 0;
+    int height = 0;
+
+    offset_x += el->padding_left;
+    offset_y += el->padding_top;
 
     if (el->type == LABEL) {
       width = sty_text(tk->renderer, tk->fsty,
                        GTA2_FONT_FSTYLE_WHITE_BLACK_NORMAL, // FIXME: selected?
                        0xffffffff, offset_x, offset_y, el->text);
+      height = 8;
     }
     if (el->type == SPRITE) {
     }
@@ -33,9 +39,12 @@ void recursive_draw(tk_t *tk, tk_el_t *el_selected, tk_el_t *el, int offset_x,
       recursive_draw(tk, el_selected, el->sub, offset_x, offset_y);
     }
 
-    // TODO: increase offset - depending on whether
-    // the parent is a STACK or FLOW
-    printf("%i\n", width);
+    if (el->parent->type == STACK)
+      offset_y += el->padding_bottom + height;
+    else if (el->parent->type == FLOW)
+      offset_x += el->padding_right + width;
+    else
+      printf("ERROR in recursive_draw: Not in a FLOW or STACK!");
 
     el = el->next;
   }
