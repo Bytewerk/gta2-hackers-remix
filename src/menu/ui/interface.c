78fe311181ef2445abe7a5d171d1bfa5784c0217
robotanarchy <robotanarchy@bingo-ev.de>
2015-04-15 13:58:58 +0200

    add credits screen

--- src/menu/ui/interface.c
+++ src/menu/ui/interface.c
@@ -7,6 +7,19 @@ ui_t *ui_init(tk_t *tk) {
   ui_t *ui = malloc(sizeof(ui_t));
   ui->tk = tk;
 
+  // CREDITS
+  tk_screen_t *credits = tk_screen(tk);
+
+  TK_STACK_SCREEN(credits, TK_PARENT->bg_mashup =
+                               bg_mashup(tk->bg, "credits", NULL, NULL, NULL);
+
+                  // TODO: scroll credits automagically,
+                  // make sure that the actionfunc gets called every
+                  // frame. Action func must also exit the program
+                  // on ESC keypress.
+                  // TK_PARENT->actionfunc = credits_action ...
+                  );
+
   // PLAY
   tk_screen_t *play = tk_screen(tk);
 
@@ -22,73 +35,13 @@ ui_t *ui_init(tk_t *tk) {
       tk_ctrl_button(TK_PARENT, "PLAY",
                      bg_mashup(tk->bg, NULL, "1_play", "1", NULL), play, NULL);
       tk_ctrl_button(TK_PARENT, "OPTIONS",
-                     bg_mashup(tk->bg, NULL, "1_options", "1", NULL), play,
+                     bg_mashup(tk->bg, NULL, "1_options", "1", NULL), NULL,
                      NULL);
       tk_ctrl_button(TK_PARENT, "QUIT",
-                     bg_mashup(tk->bg, NULL, "1_quit", "1", NULL), play,
+                     bg_mashup(tk->bg, NULL, "1_quit", "1", NULL), credits,
                      NULL););
 
-  /*
-  // Credits screen (TODO: actually display credits ;) )
-  ui->credits = tk_screen_create(tk, (void*) ui, CREDITS,
-          NULL, &ui_quit);
-  tk_screen_setbg(tk, ui->credits, "credits", NULL, NULL);
-
-
-  // Play screen
-  ui->play = tk_screen_create(tk, (void*) ui, NORMAL,NULL,NULL);
-
-  tk_control_t* name;
-  tk_control_add(ui->play, name, "PLAYER 0", TK_BUTTON, NULL);
-  tk_control_setbg(tk, name, NULL, "2_name", "2");
-  name->bottom_text_high = "ENTER:  EDIT NAME";
-  name->bottom_text_low  = "DELETE:  DELETE PLAYER";
-
-
-  tk_control_t* resume;
-  tk_control_add(ui->play, resume, "RESUME SAVED STATUS", TK_BUTTON, NULL);
-  resume->disabled = 1;
-
-  tk_control_t* scores;
-  tk_control_add(ui->play, scores, "VIEW HIGH SCORES", TK_BUTTON, NULL);
-  tk_control_setbg(tk, scores, NULL, "2_league", "2");
-
-  tk_control_t* start;
-  tk_control_add(ui->play, start, "START PLAY IN AREA", TK_CIRCLE,
-          data->value = '1';
-  );
-  tk_control_setbg(tk, start, NULL, "2_level1", "2");
-
-
-
-  // Main menu (TODO: save the version number as #define somewhere)
-ui->main = tk_screen_create(tk, (void*) ui, BOTTOM_RIGHT,
-          ui->credits, NULL);
-  ui->main->bottom_text_low = "G2HR V0.4";
-
-tk_control_t* play;
-tk_control_add(ui->main, play, UI_TEXT_PLAY, TK_BUTTON,
-          data->onclick_screen = ui->play);
-tk_control_setbg(tk, play, NULL, "1_play", "1");
-
-tk_control_t* options;
-tk_control_add(ui->main, options, UI_TEXT_OPTIONS, TK_BUTTON, NULL);
-tk_control_setbg(tk, options, NULL, "1_options", "1");
-
-tk_control_t* quit;
-tk_control_add(ui->main, quit, UI_TEXT_QUIT, TK_BUTTON,
-          data->onclick_screen = ui->credits);
-tk_control_setbg(tk, quit, NULL, "1_quit", "1");
-
-
-
-// Now that the main menu has been created, set 'back' links
-ui->play->back = ui->main;
-
-// Default screen is Main Menu
-tk->screen = ui->main;
-*/
-
+  tk->screen_active = main_menu;
   return ui;
 }
 
