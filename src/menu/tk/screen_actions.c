b509f0728d55cdee2740af4f699dab36e5975121
robotanarchy <robotanarchy@bingo-ev.de>
2015-04-15 22:57:29 +0200

    up key works

--- src/menu/tk/screen_actions.c
+++ src/menu/tk/screen_actions.c
@@ -1,6 +1,32 @@
 #include "toolkit.h"
 #include <stdio.h>
 
+void up(tk_screen_t *screen) {
+  tk_el_t *selected = screen->el_selected;
+  tk_el_t *listpos = screen->el.sub;
+  if (!selected)
+    return;
+
+  // first entry -> get the last one!
+  if (selected == listpos)
+    while (listpos->next)
+      listpos = listpos->next;
+  else
+    while (listpos && listpos->next != selected)
+      listpos = listpos->next;
+
+  screen->el_selected = listpos;
+
+  if (screen->el_selected->flags & TK_EL_FLAG_DISABLED)
+    up(screen);
+}
+
 void tk_screen_actionfunc(tk_t *tk, tk_el_t *el, tk_action_t action) {
-  printf("action: %i\n", action);
+  tk_screen_t *screen = tk->screen_active;
+
+  if (action == TK_ACTION_UP)
+    up(screen);
+
+  if (action == TK_ACTION_DOWN) {
+  }
 }
