69131ad6ebf419e1a7af9690fbf79b56ab02b7eb
robotanarchy <robotanarchy@bingo-ev.de>
2015-04-15 16:15:07 +0200

    draw backgrounds again

--- src/menu/tk/controls.c
+++ src/menu/tk/controls.c
@@ -5,7 +5,6 @@
 typedef struct {
   void *onclick_func;
   tk_screen_t *onclick_screen;
-  bg_mashup_t *bg_mashup;
 } ud_button_t;
 
 void action_button() {
@@ -19,7 +18,7 @@ void tk_ctrl_button(tk_el_t *TK_PARENT, char *text, bg_mashup_t *bg_mashup,
   ud_button_t *ud = malloc(sizeof(ud_button_t));
   ud->onclick_screen = onclick_screen;
   ud->onclick_func = onclick_func;
-  ud->bg_mashup = bg_mashup;
+  label->bg_mashup = bg_mashup;
   label->userdata = (void *)ud;
   label->actionfunc = (void *)action_button;
 }
--- src/menu/tk/screen.c
+++ src/menu/tk/screen.c
@@ -53,6 +53,32 @@ void recursive_draw(tk_t *tk, tk_el_t *el_selected, tk_el_t *el, int offset_x,
   }
 }
 
+#define DRAW_BG(WHERE, XPOS, YPOS, WIDTH, HEIGHT)                              \
+  {                                                                            \
+    if (mashup->WHERE) {                                                       \
+      SDL_Rect dstrect = {XPOS, YPOS, WIDTH, HEIGHT};                          \
+      SDL_Texture *tex =                                                       \
+          SDL_CreateTextureFromSurface(tk->renderer, mashup->WHERE->surface);  \
+      SDL_RenderCopy(tk->renderer, tex, NULL, &dstrect);                       \
+      SDL_DestroyTexture(tex);                                                 \
+    };                                                                         \
+  }
+void tk_screen_draw_bg(tk_t *tk) {
+  bg_mashup_t *mashup = tk->screen_active->el.bg_mashup;
+  tk_el_t *selected = tk->screen_active->el_selected;
+
+  if (selected && selected->bg_mashup)
+    mashup = selected->bg_mashup;
+  if (!mashup)
+    return;
+
+  DRAW_BG(full, 000, 000, 640, 480);
+  DRAW_BG(left, 000, 000, 278, 480);
+  DRAW_BG(right, 278, 000, 362, 480);
+}
+
+#undef DRAW_BG
+
 void tk_screen_draw(tk_t *tk) {
   tk_screen_t *screen = tk->screen_active;
 
@@ -61,9 +87,11 @@ void tk_screen_draw(tk_t *tk) {
     screen->el_selected = screen->el.sub;
 
   // draw background
+  tk_screen_draw_bg(tk);
 
   // draw all elements (and therefore controls)
-
   recursive_draw(tk, screen->el_selected, screen->el.sub,
                  screen->el.padding_left, screen->el.padding_top, 0);
+
+  // draw bottom text
 }
