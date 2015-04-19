4524d145d6f725147a2789195eacb810f9cd49dc
robotanarchy <robotanarchy@bingo-ev.de>
2015-04-19 15:26:52 +0200

    add title to splitscreen-screen

--- src/menu/ui/screens/splitscreen.c
+++ src/menu/ui/screens/splitscreen.c
@@ -10,11 +10,25 @@
 tk_screen_t *ui_screen_splitscreen(tk_t *tk, ui_t *ui) {
   tk_screen_t *splitscreen = tk_screen(tk, NULL, NULL);
   TK_STACK_SCREEN(
-      splitscreen, TK_PARENT->bg_mashup = bg_mashup(tk->bg, "3_tables", NULL,
-                                                    NULL, "g2hr_splitscreen");
-      tk_el_padding(TK_PARENT, 300, 161, 0, 0);
+      splitscreen,
 
-      tk_ctrl_circle(tk, TK_PARENT, "SCREEN LAYOUT", NULL, '1', '4', '1', NULL);
+      TK_PARENT->bg_mashup =
+          bg_mashup(tk->bg, "3_tables", NULL, NULL, "g2hr_splitscreen");
+
+      // title bar
+      tk_el_t *titlebar = tk_label(tk, TK_PARENT, "4 CONTROLLERS FOUND");
+      titlebar->font_id = GTA2_FONT_FSTYLE_WHITE_RED_NORMAL;
+      tk_el_padding(titlebar, 315, 12, 0, 0);
+
+      // content
+      TK_STACK(splitscreen->el_content_container = TK_PARENT;
+               tk_el_padding(TK_PARENT, 300, 123, 0, 0);
+
+               tk_el_t *circle = tk_ctrl_circle(tk, TK_PARENT, "SCREEN LAYOUT",
+                                                NULL, '1', '4', '3', NULL);
+               tk_el_padding(circle, 0, 0, 0, 30);
+
+               );
 
       );
   return splitscreen;
