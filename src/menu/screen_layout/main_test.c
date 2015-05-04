7ffa7dafc6a2e6c8a459632b4914211a569a92a8
robotanarchy <robotanarchy@bingo-ev.de>
2015-05-04 19:45:49 +0200

    start working on screen_layout parser (will have the same syntax as in the alpha version, but the parser will be written in C instead of AutoIt3)

---
+++ src/menu/screen_layout/main_test.c
@@ -0,0 +1,9 @@
+#include "screen_layout.h"
+#include <stdio.h>
+
+int main(int argc, const char **argv) {
+  if (argc != 2)
+    return printf("Syntax: main_test path/to/screen_layouts.cfg\n");
+
+  printf("Hello World!\n");
+}
---
+++ src/menu/screen_layout/screen_layout.c
@@ -0,0 +1,1 @@
+#include "screen_layout.h"
