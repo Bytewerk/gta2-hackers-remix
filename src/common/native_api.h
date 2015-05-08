509e98da00f474a4ef6d4a07a7c9d8f88516b4f0
robotanarchy <robotanarchy@bingo-ev.de>
2015-05-08 08:57:26 +0200

    improve native api some more

--- src/common/native_api.h
+++ src/common/native_api.h
@@ -1,16 +1,26 @@
 #pragma once
 #include "tk_actions.h"
-
-// NA: native api
-// WORK IN PROGRESS.
-
+/*
+        NA: native api
+*/
 #define G2HR_NATIVE_API_VERSION 1
 
+//
 // MENU TO NATIVE MESSAGES
+//
 #define NA_CLEANUP 0x01
 
-// NATIVE TO MENU MESSAGES
+// only possible, when in slotmachine mode
+#define NA_POWEROFF 0x02
+#define NA_REBOOT 0x03
 
-// send a toolkit message
-#define NA_TK_ACTION 0x01
+//
+// NATIVE TO MENU MESSAGES
+//
+
+// perform a toolkit action
+#define NA_ACTION 0x11
 typedef struct { tk_action_t action; } NA_ACTION_t;
+
+#define NA_CONTROLLERS_CONNECTED 0x12
+typedef struct { char count; } NA_CONTROLLERS_CONNECTED_t;
