af016640b0e6239070bdc3cf3f09dfa1599438e7
robotanarchy <robotanarchy@bingo-ev.de>
2015-06-05 17:57:12 +0200

    work on playing menu music. currently failss with 'WAV music output not started', FIXME.

--- src/menu/sfx/main_test.c
+++ src/menu/sfx/main_test.c
@@ -1,9 +1,29 @@
 #include "sfx.h"
 
 int main() {
+
+  if (SDL_Init(SDL_INIT_AUDIO))
+    exit(printf("SDL ERROR: %s\n", SDL_GetError()));
+
   sfx_t *sfx = sfx_init();
 
-  sfx_play(sfx, SFX_FSTYLE_CHEAT);
+  // sfx_play(sfx, SFX_FSTYLE_CHEAT);
+
+  Mix_Init(0);
+  Mix_AllocateChannels(2);
+
+  Mix_Music *song = Mix_LoadMUS("GTA2/data/GTAudio/D.wav");
+  if (!song)
+    exit(printf("Mix_LoadWAV: %s\n", Mix_GetError()));
+
+  Mix_PlayMusic(song, -1);
+
+  printf("sleeping 10s...\n");
+  SDL_Delay(10000);
+
+  Mix_FreeMusic(song);
+  Mix_Quit();
 
   sfx_cleanup(sfx);
+  SDL_Quit();
 }
--- src/menu/sfx/sfx.h
+++ src/menu/sfx/sfx.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
 /*
         These are the IDs of the GTA2 sounds in fstyle.raw.
