a2cee9418d3f4ce80b20db3bb5c25fb3d6967e5e
robotanarchy <robotanarchy@bingo-ev.de>
2015-06-07 16:18:24 +0200

    find the right sample IDs by listening to the original ones and comparing

--- src/menu/sfx/samples.h
+++ src/menu/sfx/samples.h
@@ -2,18 +2,17 @@
         These are the IDs of the GTA2 sounds in fstyle.raw.
         Lots of files sound the same (eg. 0-3), although they
         *are* different (md5'd them) so we'll just pick the first
-        one in that case.
+        one in that case. Maybe they are stereo channels? (We're just
+        playing mono here).
 
         In total, we have 17 sound samples in there. Use the sdt2wav
         tool by Black_Phoenix to convert them to WAV and listen to them
-        independently.
-
-        All of them need to be verified by directly comparing when the
-        original GTA2 menu is opened.
+        independently (or alternatively, use the main_test.c in the sfx
+        folder).
 */
 
-#define SFX_FSTYLE_RETURN 0
-#define SFX_FSTYLE_LEFT 8   // not sure
-#define SFX_FSTYLE_RIGHT 10 // not sure
-#define SFX_FSTYLE_BACK 2
+#define SFX_FSTYLE_UP_DOWN 0
+#define SFX_FSTYLE_LEFT_RIGHT 4
+#define SFX_FSTYLE_RETURN 8
+#define SFX_FSTYLE_BACK 10
 #define SFX_FSTYLE_CHEAT 16
