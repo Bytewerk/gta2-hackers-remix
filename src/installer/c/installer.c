fc4b8877c5d52e358b91b980da86024f982d48f8
robotanarchy <robotanarchy@bingo-ev.de>
2015-08-13 22:22:32 +0200

    installer extracts and executed the autoit3 gui now

--- src/installer/c/installer.c
+++ src/installer/c/installer.c
@@ -1,9 +1,11 @@
 #include "../../../src-3rdparty/xz-embedded/xz.h"
+#include "../../common/cstr/cstr.h"
 #include "packed_files.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
 typedef struct xz_dec xz_dec_t;
 typedef struct xz_buf xz_buf_t;
@@ -18,8 +20,9 @@ void extract_file(xz_dec_t *xz_dec, uint16_t index, char *output) {
   xz_buf_t xz_buf = {(uint8_t *)start,  0, end - start,
 
                      (uint8_t *)buffer, 0, size};
-  xz_dec_run(xz_dec, &xz_buf);
 
+  printf("extracting '%s' to '%s'...\n", PACKED_FILENAMES[index], output);
+  xz_dec_run(xz_dec, &xz_buf);
   FILE *handle = fopen(output, "wb");
   fwrite(buffer, size, 1, handle);
   fclose(handle);
@@ -37,9 +40,26 @@ uint16_t get_index(char *name) {
   return 0;
 }
 
-int main() {
-  xz_crc32_init();
-  xz_dec_t *xz_dec = xz_dec_init(XZ_SINGLE, 0);
-  extract_file(xz_dec, get_index("bin/g2hr.exe"), "g2hr.exe");
-  xz_dec_end(xz_dec);
+int main(int argc, char **argv) {
+  char temp[200];
+  GetTempPath(199, temp);
+
+  // default action: extract g2hr.exe and run the autoit installer,
+  // which will show a nice gui and use this installer exe-file to
+  // extract all other files, once the setup is configured.
+  if (argc == 1) {
+    char *gui = cstr_merge(temp, "\\g2hr_installer_gui.exe");
+    xz_crc32_init();
+    xz_dec_t *xz_dec = xz_dec_init(XZ_SINGLE, 0);
+    extract_file(xz_dec, get_index("bin/g2hr.exe"), gui);
+
+    char *cmd = cstr_merge(gui, " install");
+    printf("%s\n", cmd);
+    system(cmd);
+
+    // clean up
+    xz_dec_end(xz_dec);
+    free(gui);
+    free(cmd);
+  }
 }
--- src/meta/multicall.au3
+++ src/meta/multicall.au3
@@ -1,10 +1,11 @@
 #include "../installer/au3/installer.au3"
 #include "../installer/au3/uninstaller.au3"
 
+
 ; To save disk space, the autoit part of G2HR is a multicall binary now.
 ; It can act as the "meta" component, the installer or the uninstaller.
 
-If $CmdLine[0] > 1 Then
+If $CmdLine[0] >= 1 Then
 	If $CmdLine[1] == "install" Then
 		installer_main()
 		Exit 0
