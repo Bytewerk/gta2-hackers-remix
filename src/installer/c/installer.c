d22deba11cbb05758f96376457ef48ed272ac946
robotanarchy <robotanarchy@bingo-ev.de>
2015-08-15 16:26:30 +0200

    ugly win32 specific hack, since it is impossible to parse stdout in a gui program there :-\ - also use environment variables instead of commandline parameters

--- src/installer/c/installer.c
+++ src/installer/c/installer.c
@@ -10,6 +10,21 @@
 #include <string.h>
 #include <windows.h>
 
+// win32 status hack code
+// it is impossible to create a GUI program, that also can handle
+// stdout and stdin in Windows: http://stackoverflow.com/q/54536/
+// So we're using AutoIt3 anyway, let's create an invisible fake gui
+// that displays the status in the title and read that from within
+// AutoIt. FYI this "solution" is more common than you think :p
+HWND win32_status_init() {
+  return CreateWindow(NULL, "", 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL);
+}
+void win32_status_set(HWND status, char *text) {
+  SetWindowText(status, text);
+  printf("%s\n", text); // only visible in wine!
+}
+#define win32_status_cleanup(STATUS) CloseWindow(STATUS)
+
 typedef struct xz_dec xz_dec_t;
 typedef struct xz_buf xz_buf_t;
 typedef enum xz_ret xz_ret_t;
@@ -45,7 +60,7 @@ uint16_t get_index(char *name) {
   return 0;
 }
 
-int verify_hash(char *filename) {
+int verify_hash(char *filename, HWND status) {
   // open the file
   FILE *handle = fopen(filename, "rb");
   if (!handle)
@@ -112,7 +127,7 @@ void create_folder_structure(char *path) {
   cstr_split_free(split);
 }
 
-int extract_everything(char *target_root) {
+int extract_everything(char *target_root, HWND status) {
   xz_crc32_init();
   xz_dec_t *xz_dec = xz_dec_init(XZ_SINGLE, 0);
   for (int i = 0; i < PACKED_COUNT; i++) {
@@ -161,31 +176,32 @@ int gui_installer(bool cleanup, char *installer_name) {
 }
 
 int main(int argc, char **argv) {
+  char *action = getenv("G2HR_INSTALL_ACTION");
+
   // default action: extract g2hr.exe and run the autoit installer,
   // which will show a nice gui and use this installer exe-file to
   // extract all other files, once the setup is configured.
-  if (argc == 1)
+  if (!action)
     return gui_installer(false, argv[0]);
 
-  // verify the sha512 hash of the original GTA2 installer (2nd arg)
-  if (argc == 3 && !strcmp(argv[1], "verify")) {
-    verify_hash(argv[2]);
-    getchar(); // wait for the gui to parse stdout before exit
-    return 0;
-  }
-
-  // extract everything into the given folder (2nd arg)
-  if (argc == 3 && !strcmp(argv[1], "extract")) {
-    extract_everything(argv[2]);
-    getchar(); // wait for the gui to parse stdout before exit
-    return 0;
-  }
-
   // clean up the temp files extracted for the gui installer
-  if (argc == 2 && !strcmp(argv[1], "cleanup"))
+  if (!strcmp(action, "cleanup"))
     return gui_installer(true, argv[0]);
 
-  printf("invalid commandline parameters, see the source for more"
-         " info: http://git.io/g2hr\n");
-  return 1;
+  HWND status = win32_status_init();
+
+  if (!strcmp(action, "verify"))
+    verify_hash(getenv("G2HR_ORIGINAL_INSTALLER"), status);
+
+  else if (!strcmp(action, "extract"))
+    extract_everything(getenv("G2HR_EXTRACT_PATH"), status);
+
+  else
+    win32_status_set(status, "invalid action!");
+
+  // wait for the gui to parse the status before exit
+  Sleep(1000);
+
+  win32_status_cleanup(status);
+  return 0;
 }
